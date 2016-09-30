23ad48dc7097be4445ead06fe3a2ba1809f3b7e3
shaorong.gu@fangbaba.com <sharon__gu>
2016-09-30 17:02:53 +0800

    .

--- src/main/java/cloud/simple/service/UserServiceApplication.java
+++ src/main/java/cloud/simple/service/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import cloud.simple.service.conf.DataSourceProperties;
 
 @SpringBootApplication
 @RestController
+@EnableCircuitBreaker
 @EnableConfigurationProperties(DataSourceProperties.class)
 public class UserServiceApplication {
 
--- src/main/java/cloud/simple/service/domain/UserService.java
+++ src/main/java/cloud/simple/service/domain/UserService.java
@@ -1,27 +1,37 @@
 package cloud.simple.service.domain;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import cloud.simple.service.dao.UserDao;
 import cloud.simple.service.model.User;
-
-
 
 @Service
 @Transactional
 public class UserService {
-
 	
 	@Autowired
 	private UserDao userMapper;
 	
+	@HystrixCommand(fallbackMethod = "fallbackSearchAll")
 	public List<User> searchAll(){
 		List<User> list = userMapper.findAll();
 		return list;
 	}
+	
+	@SuppressWarnings("unused")
+	private List<User> fallbackSearchAll() {
+		List<User> ls = new ArrayList<User>();
+		User user = new User();
+		user.setUsername("TestHystrixCommand from userservice");
+		ls.add(user);
+		return ls;
+	}
+	
 }
