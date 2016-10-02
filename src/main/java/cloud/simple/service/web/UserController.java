c23d569b434f557acf2d6669718a3711076f3173
shaorong.gu@fangbaba.com <sharon__gu>
2016-10-02 17:13:37 +0800

    .

--- src/main/java/cloud/simple/service/web/UserController.java
+++ src/main/java/cloud/simple/service/web/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,13 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
-	public List<User> readUserInfo() {
+	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
+	public List<User> readUserInfo(@PathVariable("username") String userName) {
 		List<User> ls = userService.searchAll();
+		User u = new User();
+		u.setId(userName);
+		u.setUsername(userName);
+		ls.add(u);
 		return ls;
 	}
 }
